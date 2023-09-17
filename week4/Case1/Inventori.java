3680f7cddffd2574f89119c756283264360cd022
Muhammmad Jalaludin Qurthubi <jaxsix06@gmail.com>
2023-09-17 22:00:08 +0700

    Update Inventori.java

--- week4/Case1/Inventori.java
+++ week4/Case1/Inventori.java
@@ -1,8 +1,3 @@
-//public class Inventori {
-//    public static void main(String[] args) {
-//        System.out.println("Hello world!");
-//    }
-//}
 public class Inventori {
     Barang[] barangs;
     void initBarang() {
